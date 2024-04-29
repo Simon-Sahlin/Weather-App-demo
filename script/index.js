2e35dce748902287998a0d2235f996e4d4ee9cf3
Simon-Sahlin <simonsahlin@somin.se>
2024-04-29 20:32:30 +0200

    *transfer commit

--- script/index.js
+++ script/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+import weatherAPI from "./weatherAPI.js";
+
+async function UpdateData(){
+    let data = weatherAPI.getWeatherData("huh");
+    console.log(data);
+}
+
+UpdateData();
---
+++ script/weatherAPI.js
@@ -0,0 +1,14 @@
+
+
+
+let APIKey = "U"; //Ur gay if u steal this
+
+let weatherAPI = (function(){
+
+    async function getWeatherData(location){
+        return "yee"
+    }
+
+    return {getWeatherData};
+})();
+export default weatherAPI;
