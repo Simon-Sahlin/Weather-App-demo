e7df86f1ea91d94f92d03eb02ff46fd75f6b3778
Simon-Sahlin <simonsahlin@somin.se>
2024-05-03 11:05:04 +0200

    Fetch data from weatherAPI

--- script/index.js
+++ script/index.js
@@ -1,7 +1,8 @@
 import weatherAPI from "./weatherAPI.js";
 
 async function UpdateData(){
-    let data = weatherAPI.getWeatherData("huh");
+    let data = await weatherAPI.getWeatherData("huh");
+    console.log("Got data:")
     console.log(data);
 }
 
--- script/weatherAPI.js
+++ script/weatherAPI.js
@@ -1,12 +1,20 @@
 
 
 
-let APIKey = "U"; //Ur gay if u steal this
+let APIKey = '42638b4a1cd84e9f8c6125016242604'; //Swiper no swiping!
 
 let weatherAPI = (function(){
 
-    async function getWeatherData(location){
-        return "yee"
+    async function getWeatherData(){
+        let data = "";
+        await fetch('https://api.weatherapi.com/v1/current.json?key='+APIKey+'&q=gothenburg', {mode: 'cors'})
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(response){
+                data = response;
+            });
+        return data;
     }
 
     return {getWeatherData};
